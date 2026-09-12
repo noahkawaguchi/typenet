8388c4abf9f4f92a3350ba1a9957bca1cfe066a6
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-09-12 18:25:15 -0400

    Spawn `worker_count` worker threads

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,5 @@
 use {
+    std::{io, thread},
     typenet_server::{
         config::Config,
         server,
@@ -8,9 +9,37 @@ use {
 };
 
 /// Runs an echo server that uses a TUN device to read and write IPv4 packets: TCP, UDP, and ICMP.
+///
+/// Spawns one worker thread per `Config::worker_count`, each attached to its own queue of the same
+/// multi-queue TUN device and managing a disjoint set of TCP connections. (Since UDP and ICMP are
+/// stateless, they can be handled independently by any thread.)
 fn main() -> TraceableResult {
     let shutdown = ShutdownSignal::install()?;
     let config = Config::load()?;
-    let mut tun = tun::attach(&config.tun_name)?;
-    server::run(&mut tun, |fd, timeout| poll::readable(fd, timeout), || shutdown.load(), &config)
+
+    let mut tuns = (0..config.worker_count.get())
+        .map(|_| tun::attach(&config.tun_name))
+        .collect::<TraceableResult<Vec<_>>>()?;
+
+    thread::scope(|scope| {
+        let handles = tuns
+            .iter_mut()
+            .map(|tun| {
+                scope.spawn(|| {
+                    server::run(
+                        tun,
+                        |fd, timeout| poll::readable(fd, timeout),
+                        || shutdown.load(),
+                        &config,
+                    )
+                })
+            })
+            .collect::<Vec<_>>();
+
+        handles.into_iter().try_for_each(|handle| {
+            handle
+                .join()
+                .unwrap_or_else(|_| Err(io::Error::other("Worker thread panicked").into()))
+        })
+    })
 }
