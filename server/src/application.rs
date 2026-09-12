cd9c692de869eece2a5ba9bcbc71367e4800cac9
Noah Kawaguchi <167943427+noahkawaguchi@users.noreply.github.com>
2026-09-12 13:51:01 -0400

    Add `ShoutApp` to server

--- server/src/application.rs
+++ server/src/application.rs
@@ -1,7 +1,6 @@
 use {std::borrow::Cow, typenet_stack::application::Application};
 
-/// The `Application` this server currently runs, echoing back exactly what it receives for both UDP
-/// and TCP.
+/// A `Application` that echoes back exactly what it receives.
 pub struct EchoApp;
 
 impl Application for EchoApp {
@@ -12,27 +11,56 @@ impl Application for EchoApp {
     }
 }
 
+/// An `Application` that capitalizes all ASCII.
+pub struct ShoutApp;
+
+impl Application for ShoutApp {
+    fn handle_udp<'a>(&mut self, payload: Cow<'a, [u8]>) -> Cow<'a, [u8]> {
+        Cow::Owned(payload.iter().map(u8::to_ascii_uppercase).collect())
+    }
+
+    fn handle_tcp(&mut self, data: &[u8], send_buffer: &mut impl Extend<u8>) {
+        send_buffer.extend(data.iter().map(u8::to_ascii_uppercase));
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use {super::*, pretty_assertions::assert_eq};
+    use {
+        super::*,
+        pretty_assertions::{assert_eq, assert_matches},
+    };
 
     #[test]
-    fn handle_udp_echoes_without_allocating() {
+    fn echo_udp_echoes_without_allocating() {
         const PAYLOAD: &[u8] = b"Hello!!!";
 
-        assert_eq!(
-            EchoApp.handle_udp(Cow::Borrowed(PAYLOAD)),
-            Cow::Borrowed(PAYLOAD),
-            "Should not allocate for a trivial echo"
-        );
+        let echo = EchoApp.handle_udp(Cow::Borrowed(PAYLOAD));
+
+        // Check both `assert_eq` and `assert_matches` because borrowed and owned `Cow` are
+        // considered equal for equal content
+        assert_eq!(echo, PAYLOAD);
+        assert_matches!(echo, Cow::Borrowed(_), "Should not allocate for a trivial echo");
     }
 
     #[test]
-    fn handle_tcp_appends_data_to_send_buffer() {
-        let mut send_buffer = Vec::from(*b"AB");
+    fn echo_tcp_appends_data_to_send_buffer() {
+        let mut send_buffer = Vec::from(b"Hello");
+        EchoApp.handle_tcp(b"World", &mut send_buffer);
+        assert_eq!(send_buffer, b"HelloWorld");
+    }
 
-        EchoApp.handle_tcp(b"CD", &mut send_buffer);
+    #[test]
+    fn shout_udp_capitalizes_and_allocates() {
+        let shout = ShoutApp.handle_udp(Cow::Borrowed(b"Hello!!!"));
+        assert_eq!(shout, Vec::from(b"HELLO!!!"));
+        assert_matches!(shout, Cow::Owned(_), "Should allocate when transforming input");
+    }
 
-        assert_eq!(send_buffer, b"ABCD");
+    #[test]
+    fn shout_tcp_appends_capitalized_data_to_send_buffer() {
+        let mut send_buffer = Vec::from(b"Hello");
+        ShoutApp.handle_tcp(b"World", &mut send_buffer);
+        assert_eq!(send_buffer, b"HelloWORLD");
     }
 }
